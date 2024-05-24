59e59c0812391b63709e27c6b7534ae5d7ac2e0b
firmament1005 <daviesrueben@gmail.com>
2024-05-24 04:24:33 -0700

    Updated Nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import 'preline/preline'
 import Route from './routes';
-
 
 const App: React.FC = () => {
   return <Route />;
--- src/components/Layout/header.tsx
+++ src/components/Layout/header.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
-import { GrClose } from "react-icons/gr";
-import { GiHamburgerMenu } from "react-icons/gi";
+import React from 'react';
 
 const Header: React.FC = () => {
-    const [showMenu, setShowMenu] = useState(false);
-
     return (
         <header>
             <nav className="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -15,7 +11,7 @@ const Header: React.FC = () => {
                                 <svg id="toggleSidebarMobileHamburger" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                                 <svg id="toggleSidebarMobileClose" className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </button>
-                            <select id="large" className="flex items-center sm:mx-10 h-10 px-2 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <select id="large" className="flex cursor-pointer items-center sm:mx-10 h-10 px-2 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="store_1">Store 1</option>
                                 <option value="store_2">Store 2</option>
                                 <option value="store_3">Store 3</option>
